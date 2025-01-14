31980044c2939a6747777e6b377ed7c8356b938b
Bryan VAN PAEMEL <bryan.vanpaemel@gmail.com>
2025-01-14 07:07:15 +0000

    Add(day04) Day04_P2 Done !

--- src/day04/day04.zig
+++ src/day04/day04.zig
@@ -87,6 +87,57 @@ fn partOne(allocator: Allocator, input: []const u8) !usize {
     return total;
 }
 
+fn diag_check(diag: [2][2]i32, lines: ArrayList([]const u8), context: *Ctx) bool {
+    var mas = [_]bool{false} ** 256;
+    context.next_pos(diag[0]);
+    if (!context.check_bounds())
+        return false;
+    var ch = lines.items[@as(usize, @intCast(context.i))][@as(usize, @intCast(context.j))];
+    mas[ch] = true;
+    context.next_pos(diag[1]);
+    if (!context.check_bounds())
+        return false;
+    ch = lines.items[@as(usize, @intCast(context.i))][@as(usize, @intCast(context.j))];
+    mas[ch] = true;
+    return (mas['M'] == true and mas['S'] == true);
+}
+
+fn partTwo(allocator: Allocator, input: []const u8) !usize {
+    const lines = try splitLine(allocator, input);
+    const line_height = lines.items.len;
+    const line_width = lines.items[0].len;
+
+    const diag_left = [2][2]i32{
+        [_]i32{ -1, -1 }, [_]i32{ 2, 2 },
+    };
+    const diag_right = [2][2]i32{
+        [_]i32{ 1, -1 }, [_]i32{ -2, 2 },
+    };
+
+    var total: usize = 0;
+
+    for (0..lines.items.len) |i| {
+        const line = lines.items[i];
+        for (0..line.len) |j| {
+            if (line[j] == 'A') {
+                var context = Ctx{
+                    .width = @as(i32, @intCast(line_width)),
+                    .height = @as(i32, @intCast(line_height)),
+                    .i = @as(i32, @intCast(i)),
+                    .j = @as(i32, @intCast(j)),
+                };
+                // print("found A at: {d},{d}\n", .{ i, j });
+                const diag_left_found = diag_check(diag_left, lines, &context);
+                context.next_pos(diag_left[0]);
+                const diag_right_found = diag_check(diag_right, lines, &context);
+                if (diag_left_found and diag_right_found)
+                    total += 1;
+            }
+        }
+    }
+    return total;
+}
+
 pub fn main() !void {
     var general_purpose_allocator: std.heap.GeneralPurposeAllocator(.{}) = .init;
     const gpa = general_purpose_allocator.allocator();
@@ -103,15 +154,15 @@ pub fn main() !void {
     const result_part_one = try partOne(gpa, p1_input);
     print("Part one result: {d}\n", .{result_part_one});
 
-    // const p2_example_input = try openAndRead(page_allocator, "./src/day03/p2_example.txt");
-    // defer page_allocator.free(p2_example_input); // Free the allocated memory after use
-    //
-    // const p2_input = try openAndRead(page_allocator, "./src/day03/p2_input.txt");
-    // defer page_allocator.free(p2_input); // Free the allocated memory after use
-    //
-    // const result_part_two_example = try partTwo(p2_example_input);
-    // print("Part two example result: {d}\n", .{result_part_two_example});
-    //
-    // const result_part_two = try partTwo(p2_input);
-    // print("Part two result: {d}\n", .{result_part_two});
+    const p2_example_input = try openAndRead(page_allocator, "./src/day04/p1_example.txt");
+    defer page_allocator.free(p2_example_input); // Free the allocated memory after use
+
+    const p2_input = try openAndRead(page_allocator, "./src/day04/p2_input.txt");
+    defer page_allocator.free(p2_input); // Free the allocated memory after use
+
+    const result_part_two_example = try partTwo(gpa, p2_example_input);
+    print("Part two example result: {d}\n", .{result_part_two_example});
+
+    const result_part_two = try partTwo(gpa, p2_input);
+    print("Part two result: {d}\n", .{result_part_two});
 }
