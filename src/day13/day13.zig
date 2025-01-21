08a47ef16a7cf8195077de9643bc323da234bc99
Bryan VAN PAEMEL <bryan.vanpaemel@gmail.com>
2025-01-21 18:50:37 +0000

    Refactor(day13)

--- src/day13/day13.zig
+++ src/day13/day13.zig
@@ -18,6 +18,11 @@ fn openAndRead(path: []const u8, allocator: Allocator) ![]u8 {
 const CostA = 3;
 const CostB = 1;
 
+const Part = enum {
+    ONE,
+    TWO,
+};
+
 const Point = struct {
     x: i64,
     y: i64,
@@ -46,16 +51,20 @@ fn parseButton(line: []const u8) !Point {
     return .{ .x = buttonX, .y = buttonY };
 }
 
-fn parsePrize(line: []const u8) !Point {
+fn parsePrize(line: []const u8, p: Part) !Point {
     const fequal = std.mem.indexOfPos(u8, line, 0, "=").?;
     const vir = std.mem.indexOfPos(u8, line, 0, ",").?;
-    const prizeX = try std.fmt.parseInt(i64, line[fequal + 1 .. vir], 10);
+    var prizeX = try std.fmt.parseInt(i64, line[fequal + 1 .. vir], 10);
     const sequal = std.mem.indexOfPos(u8, line, fequal + 1, "=").?;
-    const prizeY = try std.fmt.parseInt(i64, line[sequal + 1 ..], 10);
+    var prizeY = try std.fmt.parseInt(i64, line[sequal + 1 ..], 10);
+    if (p == Part.TWO) {
+        prizeX += 10000000000000;
+        prizeY += 10000000000000;
+    }
     return .{ .x = prizeX, .y = prizeY };
 }
 
-fn parseMachine(machine: []const u8) !Context {
+fn parseMachine(machine: []const u8, p: Part) !Context {
     var lines = std.mem.tokenizeScalar(u8, machine, '\n');
     const lineA = lines.next().?;
     const lineB = lines.next().?;
@@ -63,7 +72,7 @@ fn parseMachine(machine: []const u8) !Context {
 
     const buttonA = try parseButton(lineA);
     const buttonB = try parseButton(lineB);
-    const prize = try parsePrize(linePrize);
+    const prize = try parsePrize(linePrize, p);
     return .{
         .buttonA = buttonA,
         .buttonB = buttonB,
@@ -74,14 +83,12 @@ fn parseMachine(machine: []const u8) !Context {
 fn isSolvable(ctx: Context) !bool {
     const gcdX = std.math.gcd(@as(u64, @intCast(ctx.buttonA.x)), @as(u64, @intCast(ctx.buttonB.x)));
     const gcdY = std.math.gcd(@as(u64, @intCast(ctx.buttonA.y)), @as(u64, @intCast(ctx.buttonB.y)));
-    const floatSolutionX: f32 = @as(f32, @floatFromInt(ctx.prize.x)) / @as(f32, @floatFromInt(gcdX));
-    const floatSolutionY: f32 = @as(f32, @floatFromInt(ctx.prize.y)) / @as(f32, @floatFromInt(gcdY));
-    const intSolutionX: f32 = @as(f32, @floatFromInt(@as(u64, @intFromFloat(floatSolutionX))));
-    const intSolutionY: f32 = @as(f32, @floatFromInt(@as(u64, @intFromFloat(floatSolutionY))));
-    if (intSolutionX != floatSolutionX or intSolutionY != floatSolutionY) {
-        return false;
+
+    if (@mod(ctx.prize.x, @as(i64, @intCast(gcdX))) == 0 and @mod(ctx.prize.y, @as(i64, @intCast(gcdY))) == 0) {
+        print("gcdX: {d}, gcdY: {d}\n", .{ gcdX, gcdY });
+        return true;
     }
-    return true;
+    return false;
 }
 
 fn dfs(allocator: Allocator, ctx: *Context) !usize {
@@ -89,27 +96,19 @@ fn dfs(allocator: Allocator, ctx: *Context) !usize {
     defer solutions.deinit();
 
     for (0..100) |nA| {
-        var map = AutoHashMap(i64, bool).init(allocator);
-        defer map.deinit();
+        const first = (ctx.buttonA.x * @as(i64, @intCast(nA))) + (ctx.buttonB.x);
+        const diff = ctx.prize.x - first;
+        if (@mod(diff, ctx.buttonB.x) == 0) {
+            const acutal_nb = @divExact(diff, ctx.buttonB.x) + 1;
+            if (acutal_nb <= 0 or acutal_nb > 100) continue;
 
-        for (1..100) |nB| {
-            ctx.claw.x = (ctx.buttonA.x * @as(i64, @intCast(nA))) + (ctx.buttonB.x * @as(i64, @intCast(nB)));
-            ctx.claw.y = (ctx.buttonA.y * @as(i64, @intCast(nA))) + (ctx.buttonB.y * @as(i64, @intCast(nB)));
-            const modNb = @mod(ctx.claw.x, ctx.prize.x);
+            ctx.claw.y = (ctx.buttonA.y * @as(i64, @intCast(nA))) + (ctx.buttonB.y * @as(i64, @intCast(acutal_nb)));
+            if (ctx.claw.y != ctx.prize.y) continue;
 
-            if (modNb == 0 and ctx.claw.y == ctx.prize.y) {
-                try solutions.append(.{ nA, nB });
-                break;
-            }
-
-            if (map.get(modNb) != null) {
-                break;
-            }
-
-            try map.put(modNb, true);
+            try solutions.append(.{ nA, @as(usize, @intCast(acutal_nb)) });
         }
     }
-    // print("solutions: {any}\n", .{solutions.items});
+    print("solutions: {any}\n", .{solutions.items});
 
     var min_cost: usize = if (solutions.items.len != 0) solutions.items[0][0] * 3 + solutions.items[0][1] else 0;
     for (solutions.items) |item| {
@@ -127,10 +126,25 @@ fn partOne(allocator: Allocator, input: []u8) !usize {
     var total_tokens: usize = 0;
     while (it.next()) |machine| {
         // print("{s}\n", .{machine});
-        var ctx = try parseMachine(machine);
-        // print("A: {d},{d} B: {d},{d P: {d},{d}\n", .{ ctx.buttonA.x, ctx.buttonA.y, ctx.buttonB.x, ctx.buttonB.y, ctx.prize.x, ctx.prize.y });
+        var ctx = try parseMachine(machine, Part.ONE);
         const solvable = try isSolvable(ctx);
         if (solvable) {
+            print("A: {d},{d} B: {d},{d} P: {d},{d}\n", .{ ctx.buttonA.x, ctx.buttonA.y, ctx.buttonB.x, ctx.buttonB.y, ctx.prize.x, ctx.prize.y });
+            total_tokens += try dfs(allocator, &ctx);
+        }
+    }
+    return total_tokens;
+}
+
+fn partTwo(allocator: Allocator, input: []u8) !usize {
+    var it = std.mem.tokenizeSequence(u8, input, "\n\n");
+    var total_tokens: usize = 0;
+    while (it.next()) |machine| {
+        // print("{s}\n", .{machine});
+        var ctx = try parseMachine(machine, Part.TWO);
+        const solvable = try isSolvable(ctx);
+        if (solvable) {
+            print("A: {d},{d} B: {d},{d} P: {d},{d}\n", .{ ctx.buttonA.x, ctx.buttonA.y, ctx.buttonB.x, ctx.buttonB.y, ctx.prize.x, ctx.prize.y });
             total_tokens += try dfs(allocator, &ctx);
         }
     }
@@ -150,14 +164,14 @@ pub fn main() !void {
     const result_part_one_example = try partOne(gpa, p1_example_input);
     print("Part one example result: {d}\n", .{result_part_one_example});
 
-    const result_part_one = try partOne(gpa, p1_input);
-    print("Part one result: {d}\n", .{result_part_one});
-    //
+    // const result_part_one = try partOne(gpa, p1_input);
+    // print("Part one result: {d}\n", .{result_part_one});
+
     // const p2_input = try openAndRead("./src/day12/p1_input.txt", page_allocator);
     // defer page_allocator.free(p2_input); // Free the allocated memory after use
     //
-    // const result_part_two_example = try partTwo(gpa, p1_example_input);
-    // print("Part two example result: {d}\n", .{result_part_two_example});
+    const result_part_two_example = try partTwo(gpa, p1_example_input);
+    print("Part two example result: {d}\n", .{result_part_two_example});
     //
     // const result_part_two = try partTwo(gpa, p2_input);
     // print("Part two result: {d}\n", .{result_part_two});
