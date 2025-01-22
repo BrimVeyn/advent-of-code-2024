8e0486f03378ec8a47b59384e40e9fe90b61edc7
Bryan Van paemel <bryan.vanpaemel@gmail.com>
2025-01-22 16:31:40 +0100

    Add(day13) Day13_P2 Done !

--- src/day13/day13.zig
+++ src/day13/day13.zig
@@ -23,6 +23,11 @@ const Part = enum {
     TWO,
 };
 
+const Coord = enum {
+    X,
+    Y,
+};
+
 const Point = struct {
     x: i64,
     y: i64,
@@ -88,22 +93,86 @@ fn modInverse(a: u128, mod: u128) u128 {
     return 0;
 }
 
-fn trySolve(ctx: Context, const_A: i128, const_B: i128, kA: i128, kB: i128) void {
+fn trySolve(ctx: Context, const_A: i128, const_B: i128, kA: i128, kB: i128) i128 {
     const bAx128: i128 = @intCast(ctx.buttonA.x);
     const bAy128: i128 = @intCast(ctx.buttonA.y);
     const bBx128: i128 = @intCast(ctx.buttonB.x);
     const bBy128: i128 = @intCast(ctx.buttonB.y);
-    for (0..10) |k| {
+    for (1..2) |k| {
         const nA = (kA * k) + const_A;
         const nB = const_B - (kB * k);
         const resX = nA * bAx128 + nB * bBx128;
         const resY = nA * bAy128 + nB * bBy128;
-        print("resX: {d} | resY: {d}\n", .{ resX, resY });
+        _ = resX;
+        // print("resX: {d} | resY: {d}\n", .{ resX, resY });
+
+        const prizeYi128: i128 = @intCast(ctx.prize.y);
+        // print("{d}\n", .{resY});
+        const variation = (kA * bAy128) - (kB * bBy128);
+        const platPrizeY = (resY) - prizeYi128;
+        // print("var: {d}\n", .{variation});
+        // print("goal: {d}\n", .{platPrizeY});
+        // print("{d}\n", .{prizeYi128 - (const_B * bBy128)});
+        // print("MOD: {d}\n", .{@mod(platPrizeY, variation)});
+        if (@mod(platPrizeY, variation) == 0) {
+            // print("solution !\n", .{});
+            const kY = @divExact(platPrizeY, variation);
+            print("DIST to solution: {d}\n", .{kY});
+            const _nA = const_A + (kA * -(kY - 1));
+            const _nB = const_B - (kB * -(kY - 1));
+            print("Solution: [{d}][{d}]\n", .{ const_A + (kA * -(kY - 1)), const_B - (kB * -(kY - 1)) });
+            return _nA * 3 + _nB;
+            // const _nA = (kA * kY) + const_A;
+            // const _nB = const_B - (kB * kY);
+            // const _resX = _nA * bAx128 + _nB * bBx128;
+            // const _resY = _nA * bAy128 + _nB * bBy128;
+            // print("REAL resX: {d} | resY: {d}\n", .{ _resX, _resY });
+            // print("target: {d}\n", .{resY * -kY});
+        }
         // print("resX: {d}\n", .{constant_a * @as(u128, @intCast(ctx.buttonA.x)) + constant_b * @as(u128, @intCast(ctx.buttonB.x))});
     }
+    return 0;
 }
 
-fn isSolvable(ctx: Context) !bool {
+fn solveDiophantienne(
+    ctx: Context,
+    coord: Coord,
+) !struct { u128, u128, u128, u128 } {
+    const bAcoo = if (coord == Coord.X) ctx.buttonA.x else ctx.buttonA.y;
+    const bBcoo = if (coord == Coord.X) ctx.buttonB.x else ctx.buttonB.y;
+    const prizeCoo = if (coord == Coord.X) ctx.prize.x else ctx.prize.y;
+
+    const modulo = bBcoo;
+    const prizeXrem = @rem(prizeCoo, modulo);
+    const aXrem = @rem(bAcoo, modulo);
+    // print("{d} - {d}a = 0 [{d}]\n", .{ prizeXrem, aXrem, modulo });
+
+    const gcdaXprizeX = std.math.gcd(@as(u64, @intCast(aXrem)), @as(u64, @intCast(modulo)));
+    const simpPrizeX = @divExact(@as(u128, @intCast(prizeXrem)), @as(u128, @intCast(gcdaXprizeX)));
+    const simpaX = @divExact(@as(u128, @intCast(aXrem)), @as(u128, @intCast(gcdaXprizeX)));
+    const kA = @divExact(@as(u128, @intCast(modulo)), @as(u128, @intCast(gcdaXprizeX)));
+    // print("{d}nA = {d} [{d}]\n", .{ simpaX, simpPrizeX, simpModulo });
+
+    const simpAinverse = modInverse(simpaX, kA);
+    // print("nA = {d} * {d} [{d}]\n", .{ simpPrizeX, simpAinverse, simpModulo });
+
+    // const char: u8 = if (coord == Coord.X) 'X' else 'Y';
+    const constant_a = @mod(simpPrizeX * simpAinverse, kA);
+    // print("nA{c} = {d} + {d}k\n", .{ char, constant_a, kA });
+
+    const kB = @as(u128, @intCast(bAcoo)) * kA / @as(u128, @intCast(bBcoo));
+    const constant_b = (@as(u128, @intCast(prizeCoo)) - (@as(u128, @intCast(bAcoo)) * constant_a)) / @as(u128, @intCast(bBcoo));
+    // print("nB{c} = {d} - {d}k\n", .{ char, constant_b, kB });
+
+    return .{
+        constant_a,
+        kA,
+        constant_b,
+        kB,
+    };
+}
+
+fn isSolvable(ctx: Context, total: *i128) !bool {
     const gcdX = std.math.gcd(@as(u64, @intCast(ctx.buttonA.x)), @as(u64, @intCast(ctx.buttonB.x)));
     const gcdY = std.math.gcd(@as(u64, @intCast(ctx.buttonA.y)), @as(u64, @intCast(ctx.buttonB.y)));
 
@@ -112,34 +181,35 @@ fn isSolvable(ctx: Context) !bool {
         return false;
     }
 
+    // const cAx, const nAx, const cBx, const nBx = try solveDiophantienne(ctx, Coord.X);
+    // const cAy, const nAy, const cBy, const nBy = try solveDiophantienne(ctx, Coord.Y);
+    _ = try solveDiophantienne(ctx, Coord.Y);
+    _ = try solveDiophantienne(ctx, Coord.X);
+
     const modulo = ctx.buttonB.x;
     const prizeXrem = @rem(ctx.prize.x, modulo);
     const aXrem = @rem(ctx.buttonA.x, modulo);
-
-    print("{d} - {d}a = 0 [{d}]\n", .{ prizeXrem, aXrem, modulo });
+    // print("{d} - {d}a = 0 [{d}]\n", .{ prizeXrem, aXrem, modulo });
 
     const gcdaXprizeX = std.math.gcd(@as(u64, @intCast(aXrem)), @as(u64, @intCast(modulo)));
     const simpPrizeX = @divExact(@as(u128, @intCast(prizeXrem)), @as(u128, @intCast(gcdaXprizeX)));
     const simpaX = @divExact(@as(u128, @intCast(aXrem)), @as(u128, @intCast(gcdaXprizeX)));
     const simpModulo = @divExact(@as(u128, @intCast(modulo)), @as(u128, @intCast(gcdaXprizeX)));
-    print("{d}nA = {d} [{d}]\n", .{ simpaX, simpPrizeX, simpModulo });
+    // print("{d}nA = {d} [{d}]\n", .{ simpaX, simpPrizeX, simpModulo });
 
     const simpAinverse = modInverse(simpaX, simpModulo);
-
-    print("nA = {d} * {d} [{d}]\n", .{ simpPrizeX, simpAinverse, simpModulo });
+    // print("nA = {d} * {d} [{d}]\n", .{ simpPrizeX, simpAinverse, simpModulo });
 
     const constant_a = @mod(simpPrizeX * simpAinverse, simpModulo);
-    print("nA = {d}k + {d}\n", .{ simpModulo, constant_a });
+    print("nA = {d} + {d}k\n", .{ constant_a, simpModulo });
 
     const kB = @as(u128, @intCast(ctx.buttonA.x)) * simpModulo / @as(u128, @intCast(ctx.buttonB.x));
     const constant_b = (@as(u128, @intCast(ctx.prize.x)) - (@as(u128, @intCast(ctx.buttonA.x)) * constant_a)) / @as(u128, @intCast(ctx.buttonB.x));
     print("nB = {d} - {d}k\n", .{ constant_b, kB });
 
-    trySolve(ctx, @as(i128, @intCast(constant_a)), @as(i128, @intCast(constant_b)), @as(i128, @intCast(simpModulo)), @as(i128, @intCast(kB)));
-    // print("resX: {d}\n", .{constant_a * @as(u128, @intCast(ctx.buttonA.x)) + constant_b * @as(u128, @intCast(ctx.buttonB.x))});
-    // print("resY: {d}\n", .{constant_a * @as(u128, @intCast(ctx.buttonA.y)) + constant_b * @as(u128, @intCast(ctx.buttonB.y))});
+    total.* += trySolve(ctx, @as(i128, @intCast(constant_a)), @as(i128, @intCast(constant_b)), @as(i128, @intCast(simpModulo)), @as(i128, @intCast(kB)));
 
-    return false;
+    return true;
 }
 
 fn dfs(allocator: Allocator, ctx: *Context) !usize {
@@ -178,7 +248,8 @@ fn partOne(allocator: Allocator, input: []u8) !usize {
     while (it.next()) |machine| {
         // print("{s}\n", .{machine});
         var ctx = try parseMachine(machine, Part.ONE);
-        const solvable = try isSolvable(ctx);
+        var subtotal: i128 = 0;
+        const solvable = try isSolvable(ctx, &subtotal);
         if (solvable) {
             print("A: {d},{d} B: {d},{d} P: {d},{d}\n", .{ ctx.buttonA.x, ctx.buttonA.y, ctx.buttonB.x, ctx.buttonB.y, ctx.prize.x, ctx.prize.y });
             total_tokens += try dfs(allocator, &ctx);
@@ -187,19 +258,20 @@ fn partOne(allocator: Allocator, input: []u8) !usize {
     return total_tokens;
 }
 
-fn partTwo(allocator: Allocator, input: []u8) !usize {
+fn partTwo(allocator: Allocator, input: []u8) !i128 {
     var it = std.mem.tokenizeSequence(u8, input, "\n\n");
     var total_tokens: usize = 0;
+    var subtotal: i128 = 0;
     while (it.next()) |machine| {
         // print("{s}\n", .{machine});
         var ctx = try parseMachine(machine, Part.TWO);
-        const solvable = try isSolvable(ctx);
+        // print("A: {d},{d} B: {d},{d} P: {d},{d}\n", .{ ctx.buttonA.x, ctx.buttonA.y, ctx.buttonB.x, ctx.buttonB.y, ctx.prize.x, ctx.prize.y });
+        const solvable = try isSolvable(ctx, &subtotal);
         if (solvable) {
-            print("A: {d},{d} B: {d},{d} P: {d},{d}\n", .{ ctx.buttonA.x, ctx.buttonA.y, ctx.buttonB.x, ctx.buttonB.y, ctx.prize.x, ctx.prize.y });
             total_tokens += try dfs(allocator, &ctx);
         }
     }
-    return total_tokens;
+    return subtotal;
 }
 
 pub fn main() !void {
@@ -223,9 +295,9 @@ pub fn main() !void {
     //
     const result_part_two_example = try partTwo(gpa, p1_example_input);
     print("Part two example result: {d}\n", .{result_part_two_example});
-    //
-    // const result_part_two = try partTwo(gpa, p2_input);
-    // print("Part two result: {d}\n", .{result_part_two});
+
+    const result_part_two = try partTwo(gpa, p1_input);
+    print("Part two result: {d}\n", .{result_part_two});
 
     const leaks = general_purpose_allocator.deinit();
     _ = leaks;
