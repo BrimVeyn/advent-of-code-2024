580fd3ffe03f9a762d63b2cd53c532d9fe2d7e9c
Bryan VAN PAEMEL <bryan.vanpaemel@gmail.com>
2025-01-26 17:56:20 +0000

    Refactor(day16) djikstra implemented. Need to incorporate distances in the calculation :(

--- src/day16/day16.zig
+++ src/day16/day16.zig
@@ -20,30 +20,16 @@ fn openAndRead(path: []const u8, allocator: Allocator) ![]u8 {
     return file_content;
 }
 
-const Context = struct {
-    maze: ArrayList([]u8),
-};
+const INF: usize = 9999999999;
 
-const Special = enum {
-    START,
-    FINISH,
-};
-
-const INF: usize = 9999;
-
-const Dir = enum(usize) {
-    UP,
-    RIGHT,
-    DOWN,
-    LEFT,
-};
+const Cost = [4]usize;
 
 const Node = struct {
     pos: uVec2,
-    up: struct { ?*Node, usize } = .{ null, 0 },
-    right: struct { ?*Node, usize } = .{ null, 0 },
-    down: struct { ?*Node, usize } = .{ null, 0 },
-    left: struct { ?*Node, usize } = .{ null, 0 },
+    up: struct { ?*Node, Cost } = .{ null, .{ 0, 0, 0, 0 } },
+    right: struct { ?*Node, Cost } = .{ null, .{ 0, 0, 0, 0 } },
+    down: struct { ?*Node, Cost } = .{ null, .{ 0, 0, 0, 0 } },
+    left: struct { ?*Node, Cost } = .{ null, .{ 0, 0, 0, 0 } },
 };
 
 fn parseInput(alloc: Allocator, input: []u8) !struct { [][]u8, uVec2, uVec2 } {
@@ -117,16 +103,20 @@ fn addOffset(vec: uVec2, offset: iVec2) uVec2 {
     };
 }
 
+//up, right, down, left
+
+const TURN: usize = 1000;
+
 fn linkNode(entry: Entry, rhs: *Node, dir: iVec2, dist: usize) void {
     switch (dir[1]) {
         1 => {
             entry.value_ptr.down[0] = rhs;
-            entry.value_ptr.down[1] = dist;
+            entry.value_ptr.down[1] = .{ INF, dist + TURN, dist, dist + TURN };
             return;
         },
         -1 => {
             entry.value_ptr.up[0] = rhs;
-            entry.value_ptr.up[1] = dist;
+            entry.value_ptr.up[1] = .{ dist, dist + TURN, INF, dist + TURN };
             return;
         },
         else => {},
@@ -134,12 +124,12 @@ fn linkNode(entry: Entry, rhs: *Node, dir: iVec2, dist: usize) void {
     switch (dir[0]) {
         1 => {
             entry.value_ptr.right[0] = rhs;
-            entry.value_ptr.right[1] = dist;
+            entry.value_ptr.right[1] = .{ dist + TURN, dist, dist + TURN, INF };
             return;
         },
         -1 => {
             entry.value_ptr.left[0] = rhs;
-            entry.value_ptr.left[1] = dist;
+            entry.value_ptr.left[1] = .{ dist + TURN, INF, dist + TURN, dist };
             return;
         },
         else => {},
@@ -204,10 +194,139 @@ fn createNodes(alloc: Allocator, maze: [][]u8) !AutoArrayHashMap(uVec2, Node) {
             }
         }
     }
-
     printMapWithNodes(nodesMap, maze);
     linkNodes(nodesMap, maze);
     return nodesMap;
+}
+
+fn fillLine(node: struct { ?*Node, Cost }, nodesMap: AutoArrayHashMap(uVec2, Node), line: *ArrayList(Cost)) void {
+    if (node[0] != null) {
+        const up_idx = nodesMap.getIndex(node[0].?.pos).?;
+        line.items[up_idx] = node[1];
+    }
+}
+
+fn buildAdjMatrix(alloc: Allocator, nodesMap: AutoArrayHashMap(uVec2, Node)) ![][]Cost {
+    const nodesCount = nodesMap.count();
+    var matrix = try ArrayList([]Cost).initCapacity(alloc, nodesCount);
+
+    var it = nodesMap.iterator();
+    while (it.next()) |entry| {
+        var line = try ArrayList(Cost).initCapacity(alloc, nodesCount);
+        line.appendNTimesAssumeCapacity(.{ INF, INF, INF, INF }, nodesCount);
+        fillLine(entry.value_ptr.up, nodesMap, &line);
+        fillLine(entry.value_ptr.right, nodesMap, &line);
+        fillLine(entry.value_ptr.down, nodesMap, &line);
+        fillLine(entry.value_ptr.left, nodesMap, &line);
+        matrix.appendAssumeCapacity(try line.toOwnedSlice());
+    }
+    return try matrix.toOwnedSlice();
+}
+
+const Order = std.math.Order;
+
+const State = struct {
+    idx: usize,
+    dir: usize,
+    cost: usize,
+};
+
+fn lessThan(context: void, a: State, b: State) Order {
+    _ = context;
+    return std.math.order(a.cost, b.cost);
+}
+
+fn reconstructPath(alloc: Allocator, predecessors: [][4]?usize, start: usize, end: usize) ![]usize {
+    var path = ArrayList(usize).init(alloc);
+    var current = end;
+
+    while (current != start) {
+        try path.append(current);
+        current = predecessors[current][1] orelse break;
+    }
+    try path.append(start);
+    return try path.toOwnedSlice();
+}
+
+fn djikstra(alloc: Allocator, adjMatrix: [][]Cost, start: usize, end: usize) !struct { distance: usize, path: []usize } {
+    const nodesCount = adjMatrix.len;
+
+    var distances = try ArrayList([4]usize).initCapacity(alloc, nodesCount);
+    var visited = try ArrayList([4]bool).initCapacity(alloc, nodesCount);
+    var predecessors = try ArrayList([4]?usize).initCapacity(alloc, nodesCount);
+    var queue = std.PriorityQueue(State, void, lessThan).init(alloc, {});
+    defer {
+        predecessors.deinit();
+        distances.deinit();
+        visited.deinit();
+        queue.deinit();
+    }
+
+    predecessors.appendNTimesAssumeCapacity(.{ null, null, null, null }, nodesCount);
+    distances.appendNTimesAssumeCapacity(.{ INF, INF, INF, INF }, nodesCount);
+    visited.appendNTimesAssumeCapacity(.{ false, false, false, false }, nodesCount);
+    distances.items[start] = .{ 0, 0, 0, 0 };
+    try queue.add(.{ .idx = start, .cost = 0, .dir = 1 });
+
+    //0 == north, 1 = east, 2 = south, 3 = east
+    while (queue.count() > 0) {
+        const current = queue.remove();
+
+        if (visited.items[current.idx][current.dir] == true) continue;
+        visited.items[current.idx][current.dir] = true;
+
+        if (current.idx == end) {
+            break;
+        }
+
+        for (adjMatrix[current.idx], 0..) |dist, neighbor| {
+            if (dist[current.dir] == INF or visited.items[neighbor][current.dir]) continue;
+            for (0..4) |newDir| {
+                const tryDist = distances.items[current.idx][newDir] + dist[newDir];
+                if (tryDist < distances.items[neighbor][newDir]) {
+                    predecessors.items[neighbor][newDir] = current.idx;
+                    distances.items[neighbor][newDir] = tryDist;
+                    try queue.add(.{ .idx = neighbor, .dir = newDir, .cost = tryDist });
+                }
+            }
+        }
+    }
+    const path = try reconstructPath(alloc, predecessors.items, start, end);
+    return .{ .distance = distances.items[end][1], .path = path };
+}
+
+fn colorizePath(maze: [][]u8) void {
+    const blue = "\x1b[34m"; // ANSI code for blue
+    const reset = "\x1b[0m"; // ANSI code to reset colors}
+
+    for (maze) |line| {
+        for (line) |ch| {
+            if (ch != '+') {
+                print("{c}", .{ch});
+                continue;
+            } else {
+                print("{s}{c}{s}", .{ blue, ch, reset });
+            }
+        }
+        print("\n", .{});
+    }
+}
+
+fn printMapWithPath(nodesMap: AutoArrayHashMap(uVec2, Node), maze: [][]u8, path: []usize) void {
+    for (0..path.len - 1) |i| {
+        const from: uVec2 = nodesMap.keys()[path[i]];
+        const to: uVec2 = nodesMap.keys()[path[i + 1]];
+        const dy: i32 = @as(i32, @intCast(to[1])) - @as(i32, @intCast(from[1]));
+        const dx: i32 = @as(i32, @intCast(to[0])) - @as(i32, @intCast(from[0]));
+        // print("from {} to {}, dy {}\n", .{ from, to, dy });
+
+        const drawS = if (dy > 0 or dx > 0) to else from;
+        for (0..@abs(dy + dx) + 1) |iy| {
+            maze[drawS[1] - if (dy != 0) iy else 0][drawS[0] - if (dx != 0) iy else 0] = '+';
+        }
+    }
+    colorizePath(maze);
+    return;
 }
 
 fn partOne(alloc: Allocator, input: []u8) !usize {
@@ -216,14 +335,31 @@ fn partOne(alloc: Allocator, input: []u8) !usize {
         for (maze) |line| alloc.free(line);
         alloc.free(maze);
     }
-    // for (maze) |line| print("{s}\n", .{line});
     var nodesMap: AutoArrayHashMap(uVec2, Node) = try createNodes(alloc, maze);
     defer nodesMap.deinit();
 
-    printNodes(nodesMap);
-    _ = start;
-    _ = end;
-    return 0;
+    // printNodes(nodesMap);
+
+    const adjMatrix = try buildAdjMatrix(alloc, nodesMap);
+    defer {
+        for (adjMatrix) |line| alloc.free(line);
+        alloc.free(adjMatrix);
+    }
+
+    // for (adjMatrix) |line| {
+    //     print("{any}\n", .{line});
+    // }
+
+    const startIdx = nodesMap.getIndex(start).?;
+    const endIdx = nodesMap.getIndex(end).?;
+
+    print("start: {}, end: {}\n", .{ start, end });
+    const result = try djikstra(alloc, adjMatrix, startIdx, endIdx);
+    defer alloc.free(result.path);
+    print("path: {any}\n", .{result.path});
+    printMapWithPath(nodesMap, maze, result.path);
+
+    return result.distance;
 }
 
 pub fn main() !void {
