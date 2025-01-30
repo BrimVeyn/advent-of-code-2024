3972b29157fda21a3c74f5626c424f672010813b
Bryan VAN PAEMEL <bryan.vanpaemel@gmail.com>
2025-01-30 07:24:51 +0000

    Add(day19) day19_p1 wip

--- src/day19/day19.zig
+++ src/day19/day19.zig
@@ -18,9 +18,51 @@ fn openAndRead(path: []const u8, allocator: Allocator) ![]u8 {
     return file_content;
 }
 
+const PatternMap = std.StringHashMap(bool);
+
+const Context = struct {
+    patterns: PatternMap,
+    towels: ArrayList([]u8),
+
+    pub fn init(alloc: Allocator, input: []u8) !Context {
+        var blocks = std.mem.tokenizeSequence(u8, input, "\n\n");
+        const patBlock = blocks.next().?;
+
+        var patterns = PatternMap.init(alloc);
+        var patIt = std.mem.tokenizeScalar(u8, patBlock, ',');
+        while (patIt.next()) |it| {
+            const stripped = try alloc.dupe(u8, std.mem.trimLeft(u8, it, " "));
+            try patterns.put(stripped, true);
+        }
+
+        var towels = ArrayList([]u8).init(alloc);
+
+        const towBlock = blocks.next().?;
+        var towIt = std.mem.tokenizeScalar(u8, towBlock, '\n');
+        while (towIt.next()) |towel| {
+            try towels.append(try alloc.dupe(u8, towel));
+        }
+
+        return .{
+            .patterns = patterns,
+            .towels = towels,
+        };
+    }
+};
+
 fn partOne(alloc: Allocator, input: []u8) !usize {
-    _ = alloc;
-    _ = input;
+    var ctx = try Context.init(alloc, input);
+    defer {
+        var it = ctx.patterns.iterator();
+        while (it.next()) |entry| {
+            alloc.free(entry.key_ptr.*);
+        }
+        ctx.patterns.deinit();
+        for (ctx.towels.items) |towel| alloc.free(towel);
+        ctx.towels.deinit();
+    }
+
+    print("towels: {s}\n", .{ctx.towels.items});
     return 0;
 }
 
@@ -28,16 +70,16 @@ pub fn main() !void {
     var general_purpose_allocator: std.heap.GeneralPurposeAllocator(.{}) = .init;
     const gpa = general_purpose_allocator.allocator();
 
-    const p1_example_input = try openAndRead("./src/day18/p1_example.txt", page_allocator);
+    const p1_example_input = try openAndRead("./src/day19/p1_example.txt", page_allocator);
     defer page_allocator.free(p1_example_input); // Free the allocated memory after use
 
-    const p1_input = try openAndRead("./src/day18/p1_input.txt", page_allocator);
+    const p1_input = try openAndRead("./src/day19/p1_input.txt", page_allocator);
     defer page_allocator.free(p1_input); // Free the allocated memory after use
 
     const res_ex = try partOne(gpa, p1_example_input);
     print("Part one example result: {d}\n", .{res_ex});
 
-    // const res_real = try partOne(gpa, p1_input, 71, 1024);
+    // const res_real = try partOne(gpa, p1_input);
     // print("Part one example result: {d}\n", .{res_real});
 
     const leaks = general_purpose_allocator.deinit();
