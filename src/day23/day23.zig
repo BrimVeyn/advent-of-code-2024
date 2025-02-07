51e2cdf449c01eb9d346c4f91df9aecbd1c6acf5
Bryan Van paemel <bryan.vanpaemel@gmail.com>
2025-02-07 17:36:30 +0100

    Add(day23) day23_p2 done !

--- src/day23/day23.zig
+++ src/day23/day23.zig
@@ -36,18 +36,6 @@ pub fn parse(alloc: Allocator, input: []const u8) !Links {
     return links;
 }
 
-pub fn intersection(alloc: Allocator, lhs: ArrayList(Id), rhs: ArrayList(Id)) !ArrayList(Id) {
-    var set = ArrayList(Id).init(alloc);
-    for (lhs.items) |lv| {
-        for (rhs.items) |rv| {
-            if (std.mem.eql(u8, lv, rv)) {
-                try set.append(lv);
-            }
-        }
-    }
-    return set;
-}
-
 fn lessThan(context: void, a: Id, b: Id) bool {
     _ = context;
     if (a[0] < b[0]) return true;
@@ -77,21 +65,17 @@ const SliceContext = struct {
     }
 };
 
-const TrioContext = struct {
-    pub fn hash(ctx: TrioContext, key: [3]Id) u32 {
-        _ = ctx;
-        var h = std.hash.Fnv1a_32.init();
-        h.update(key[0]);
-        h.update(key[1]);
-        h.update(key[2]);
-        return h.final();
+pub fn intersection(alloc: Allocator, lhs: ArrayList(Id), rhs: ArrayList(Id)) !ArrayList(Id) {
+    var set = ArrayList(Id).init(alloc);
+    for (lhs.items) |lv| {
+        for (rhs.items) |rv| {
+            if (std.mem.eql(u8, lv, rv)) {
+                try set.append(lv);
+            }
+        }
     }
-
-    pub fn eql(ctx: TrioContext, a: [3]Id, b: [3]Id, _: usize) bool {
-        _ = ctx;
-        return std.mem.eql(u8, a[0], b[0]) and std.mem.eql(u8, a[1], b[1]) and std.mem.eql(u8, a[2], b[2]);
-    }
-};
+    return set;
+}
 
 pub fn partOne(alloc: Allocator, input: []const u8) !usize {
     var links = try parse(alloc, input);
@@ -101,87 +85,137 @@ pub fn partOne(alloc: Allocator, input: []const u8) !usize {
         links.deinit();
     }
 
-    //Iterate over every links, compute its intersection
     var it = links.iterator();
 
-    var trios = std.ArrayHashMap([3]Id, bool, TrioContext, true).init(alloc);
-    defer trios.deinit();
+    var trios = Intersections.init(alloc);
+    defer {
+        // var tIt = trios.iterator();
+        // while (tIt.next()) |entry| {
+        //     alloc.free(entry.key_ptr.*);
+        // }
+
+        trios.deinit();
+    }
+
+    var visited = SliceMap.init(alloc);
+    defer visited.deinit();
 
     while (it.next()) |entry| {
         print("{s}: {s}\n", .{ entry.key_ptr.*, entry.value_ptr.items });
         for (entry.value_ptr.items) |value| {
             const rhs = links.get(value).?;
+
+            var couple = [2]Id{ entry.key_ptr.*, value };
+            std.mem.sort(Id, couple[0..], {}, lessThan);
+
+            const matchVisited = try visited.getOrPutValue(couple[0..], true);
+            if (matchVisited.found_existing)
+                continue;
+
             const inter = try intersection(alloc, entry.value_ptr.*, rhs);
             defer inter.deinit();
+
             if (inter.items.len != 0) {
                 for (inter.items) |item| {
-                    var final_seq = [3]Id{ entry.key_ptr.*, value, item };
-                    if (final_seq[0][0] == 't' or
-                        final_seq[1][0] == 't' or
-                        final_seq[2][0] == 't')
-                    {
-                        // print("UnSorted: {s}\n", .{final_seq});
-                        std.sort.heap(Id, final_seq[0..], {}, lessThan);
-                        // print("Sorted: {s}\n", .{final_seq});
-                        try trios.put(final_seq, true);
-                        // print("inter: {s}(){s}: {s}\n", .{ entry.key_ptr.*, value, inter.items });
-                    }
+                    var final_seq = [2]Id{ value, item };
+                    // const key = entry.key_ptr.*;
+                    // const newValue = [2]Id{ value, item };
+                    //
+                    // print("Trio: {s}: {s}\n", .{ key, newValue });
+                    // if (final_seq[0][0] == 't' or
+                    //     final_seq[1][0] == 't' or
+                    //     final_seq[2][0] == 't')
+                    // {
+                    std.mem.sort(Id, final_seq[0..], {}, lessThan);
+                    const owned_seq = try alloc.dupe(Id, final_seq[0..]);
+
+                    const match = try trios.getOrPutValue(entry.key_ptr.*, SliceMap.init(alloc));
+                    const matchInner = try match.value_ptr.getOrPutValue(owned_seq, true);
+                    if (matchInner.found_existing)
+                        alloc.free(owned_seq);
+                    // }
                 }
             }
-            // trios += inter.items.len;
         }
     }
 
     var triosIt = trios.iterator();
     while (triosIt.next()) |entry| {
-        print("{s}\n", .{entry.key_ptr.*});
+        // print("{s}: {s}\n", .{ entry.key_ptr.*, entry.value_ptr.keys() });
+        var valueIt = entry.value_ptr.iterator();
+        while (valueIt.next()) |values| {
+            alloc.free(values.key_ptr.*);
+        }
+        entry.value_ptr.deinit();
     }
 
     return trios.count();
 }
 
-pub fn findHigherGroups(alloc: Allocator, ids: []Id, trios: std.ArrayHashMap([]Id, bool, SliceContext, true)) !usize {
-    var trioIt = trios.iterator();
+const SliceMap = std.ArrayHashMap([]Id, bool, SliceContext, true);
+const Intersections = StringArrayHashMap(std.ArrayHashMap([]Id, bool, SliceContext, true));
 
-    var currentOrder: usize = 3;
+pub fn findHigherGroups(alloc: Allocator, ids: []Id, trios: SliceMap) !usize {
+    var order: usize = 3;
+    var curOrder = trios;
     while (true) {
-        var nextOrder = std.ArrayHashMap([]Id, bool, SliceContext, true).init(alloc);
-        defer nextOrder.deinit();
+        var nextOrder = SliceMap.init(alloc);
 
-        defer currentOrder += 1;
+        defer {
+            var it = curOrder.iterator();
+            while (it.next()) |entry| {
+                alloc.free(entry.key_ptr.*);
+            }
+            curOrder.deinit();
+            curOrder = nextOrder;
+        }
 
-        while (trioIt.next()) |entry| {
+        defer order += 1;
+
+        var curIt = curOrder.iterator();
+        const curCount = curOrder.count();
+        var iiii: usize = 0;
+        while (curIt.next()) |entry| : (iiii += 1) {
+            print("{d}/{d}\n", .{ iiii, curCount });
             outer: for (ids) |id| {
-                for (0..currentOrder) |i| {
+                for (0..order) |i| {
                     var conTest = try alloc.dupe(Id, entry.key_ptr.*);
                     defer alloc.free(conTest);
                     conTest[i] = id;
-                    std.sort.heap(Id, conTest[0..], {}, lessThan);
-                    if (trios.get(conTest) != null) {} else {
+                    std.mem.sort(Id, conTest[0..], {}, lessThan);
+                    if (curOrder.get(conTest) != null) {} else {
                         continue :outer;
                     }
                 }
                 var tmp = ArrayList(Id).init(alloc);
                 defer tmp.deinit();
-                for (0..entry.key_ptr.len) |i| {
-                    try tmp.append(entry.key_ptr.*[i]);
-                }
+
+                for (0..entry.key_ptr.len) |i| try tmp.append(entry.key_ptr.*[i]);
                 try tmp.append(id);
+                std.mem.sort(Id, tmp.items[0..], {}, lessThan);
                 // print("tmp: {s}\n", .{tmp.items});
-                std.sort.heap(Id, tmp.items[0..], {}, lessThan);
 
                 const tmpAsSlice = try tmp.toOwnedSlice();
                 const match = try nextOrder.getOrPutValue(tmpAsSlice, true);
-                if (match.found_existing) alloc.free(tmpAsSlice);
-                print("Match: {s}-{s}\n", .{ entry.key_ptr.*, id });
+                if (match.found_existing)
+                    alloc.free(tmpAsSlice);
+
+                // print("Match: {s}-{s}\n", .{ entry.key_ptr.*, id });
             }
         }
-        var debug = nextOrder.iterator();
-        while (debug.next()) |entry| {
-            print("{s}\n", .{entry.key_ptr.*});
-            alloc.free(entry.key_ptr.*);
+        // var debug = nextOrder.iterator();
+        // while (debug.next()) |entry| {
+        //     print("{s}\n", .{entry.key_ptr.*});
+        // }
+
+        if (nextOrder.count() == 0) {
+            var it = nextOrder.iterator();
+            while (it.next()) |entry| {
+                alloc.free(entry.key_ptr.*);
+            }
+            nextOrder.deinit();
+            break;
         }
-        break;
     }
     return 1;
 }
@@ -197,8 +231,7 @@ pub fn partTwo(alloc: Allocator, input: []const u8) !usize {
     //Iterate over every links, compute its intersection
     var it = links.iterator();
 
-    var trios = std.ArrayHashMap([]Id, bool, SliceContext, true).init(alloc);
-    defer trios.deinit();
+    var trios = SliceMap.init(alloc);
 
     while (it.next()) |entry| {
         print("{s}: {s}\n", .{ entry.key_ptr.*, entry.value_ptr.items });
@@ -211,17 +244,14 @@ pub fn partTwo(alloc: Allocator, input: []const u8) !usize {
                     var final_seq = [3]Id{ entry.key_ptr.*, value, item };
                     std.sort.heap(Id, final_seq[0..], {}, lessThan);
 
-                    var tmp = ArrayList(Id).init(alloc);
-                    defer tmp.deinit();
-                    try tmp.appendSlice(final_seq[0..]);
-                    const owned_tmp = try tmp.toOwnedSlice();
-                    // print("UnSorted: {s}\n", .{final_seq});
-                    // print("Sorted: {s}\n", .{final_seq});
-                    try trios.put(owned_tmp, true);
+                    const owned_seq = try alloc.dupe(Id, final_seq[0..]);
+
+                    const match = try trios.getOrPutValue(owned_seq, true);
+                    if (match.found_existing)
+                        alloc.free(owned_seq);
                 }
-                print("inter: {s}(){s}: {s}\n", .{ entry.key_ptr.*, value, inter.items });
+                // print("inter: {s}(){s}: {s}\n", .{ entry.key_ptr.*, value, inter.items });
             }
-            // trios += inter.items.len;
         }
     }
 
@@ -233,7 +263,7 @@ pub fn partTwo(alloc: Allocator, input: []const u8) !usize {
     const result = try findHigherGroups(alloc, links.keys(), trios);
     _ = result;
 
-    return trios.count();
+    return 0;
 }
 
 pub fn main() !void {
@@ -243,15 +273,15 @@ pub fn main() !void {
     // const oneExample = try partOne(alloc, Example);
     // print("PartOne(Example): {any}\n", .{oneExample});
 
-    // const oneReal = try partOne(alloc, Real);
-    // print("PartOne(Real): {any}\n", .{oneReal});
+    const oneReal = try partOne(alloc, Real);
+    print("PartOne(Real): {any}\n", .{oneReal});
 
-    const twoExample = try partTwo(alloc, Example);
-    print("PartTwo(Example): {any}\n", .{twoExample});
+    // const twoExample = try partTwo(alloc, Example);
+    // print("PartTwo(Example): {any}\n", .{twoExample});
 
     // const twoReal = try partTwo(alloc, Real);
     // print("PartTwo(Real): {any}\n", .{twoReal});
 
-    // const leaks = gpa.deinit();
-    // _ = leaks;
+    const leaks = gpa.deinit();
+    _ = leaks;
 }
